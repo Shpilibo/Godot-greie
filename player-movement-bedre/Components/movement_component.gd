fb2de43369af5173f8e5c75c064705ecfd21bb3e
ComfortHeadStrap <peidesenlian@gmail.com>
2025-12-11 14:09:01 +0100

    Bare gløm den forrige

--- player-movement-bedre/Components/movement_component.gd
+++ player-movement-bedre/Components/movement_component.gd
@@ -3,7 +3,7 @@ extends Node
 
 
 @export_subgroup("settings")
-@export var speed: float = 100
+@export var speed: float = 700
 
 func handle_horizontal_movement(body: CharacterBody2D, direction: float) -> void:
 	body.velocity.x = direction * speed
